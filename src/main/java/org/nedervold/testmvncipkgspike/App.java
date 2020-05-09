f0109473bac065de264346d1b554f68614a9af56
Eric Nedervold <nedervoldsoftware@gmail.com>
2020-05-09 12:36:31 -0700

    Now can use a Maven package from GitHub.

---
+++ src/main/java/org/nedervold/testmvncipkgspike/App.java
@@ -0,0 +1,8 @@
+package org.nedervold.testmvncipkgspike;
+
+public class App {
+    public static void main(String[] main){
+	new org.nedervold.mvncipkgspike.App().service();
+	System.out.println("So long, y'all.");
+    }
+}
